e0241f20e5476fa107207cf806390a3171cdeae3
preethamsetty <preethambs2002@gmail.com>
2024-09-18 13:17:26 +0530

    [Preetham B S] Add- Added IIFE folder

---
+++ IIFE/iife.js
@@ -0,0 +1,55 @@
+// Immediately Invoked Function Expressions (IIFEs) in JavaScript
+
+(function() {
+    console.log('Aseeja');
+  })();
+
+  //------------------------------------
+
+//Example 
+
+(function() {
+    let privateVariable = "secret";
+  
+    function privateFunction() {
+      console.log(privateVariable);
+    }
+  
+    privateFunction();
+  })();
+
+// In this example, the IIFE creates a private scope, and the privateVariable and privateFunction are only 
+// accessible within that scope.
+
+//-----------------------------------------------------------------------
+
+// This IIFE creates a private counter object with methods to increment, decrement, and get the current value. 
+//The count variable remains private within the IIFE
+
+const counter = (function() {
+    let count = 0;
+  
+    return {
+      increment: function() {
+        count++;
+      },
+      decrement: function() {
+        count--;
+      },
+      getValue: function() {
+        return count;
+      }
+    };
+  })();
+  
+  counter.increment();   //1
+  counter.increment();   //2
+  counter.decrement();   //1
+  counter.increment();   //2
+  counter.decrement();   //1
+  console.log(counter.getValue()); // Output: 1
+
+  //----------------------------------------------------------------------
+
+  
+
