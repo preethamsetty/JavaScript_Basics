597916f63fae65ef91b6771aefbdb454d24389b7
preethamsetty <preethambs2002@gmail.com>
2024-09-18 10:24:58 +0530

    [Preetham B S] add- Added changes to ES9 folder

--- ES9/basics.js
+++ ES9/basics.js
@@ -22,4 +22,22 @@ console.log(re.test('hello\nworld')); // true
 
 // ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$   (email validation)
 
+//-------------------------------------------------------------------
+let str = "Hello World";
+let regex = /World/;
+console.log(regex.test(str)); // Output: true
+
+//-------------------------------------------------------------------
+
+//Case Insensitive Match
+let str1 = "hello world";
+let regex1 = /Hello/i;
+console.log(regex.test(str)); // Output: true
+
+//---------------------------------------------------------------------------
+
+
+
+
+
 
